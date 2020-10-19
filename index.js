58a1b47b3bfe905d698e84eb9ef3174410357072
JunjPan <elisepjj@hotmail.com>
2020-10-19 15:18:14 -0700

    heroku configuration

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path=require('path');
 
 const app=express();
 
-if(process.env.NODE_ENV==='production'){
+
     app.use(express.static(path.join(__dirname,"client/build")));
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,"client","build","index.html"))
     })
-}
+
 
 const port=process.env.PORT||5000;
 app.listen(port,()=>{
